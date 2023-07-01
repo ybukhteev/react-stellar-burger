34b0623c6055eaf05269bf90a350c18643593afe
ybukhteev <mmm424@yandex.ru>
2023-07-01 19:47:27 +0300

    added class Api

---
+++ src/components/api/api.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Api extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.baseUrl = 'https://norma.nomoreparties.space/api/ingredients'
+  }
+}
+
+getResources = async (url) => {
+  const res = await fetch(url);
+
+  if (res.ok) {
+    return await res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+};
+
+getIngridients = () => {
+  return this.getResources(`${this.baseUrl}`)
+}
+
+export default Api;
